5a9362b38897e3eaa256e622e800a975e2abfe83
Sirah Tabarez <sartab2011@gmail.com>
2022-12-16 17:40:40 +0100

    iteration 1 - 5 not showing in MongoDB

--- index.js
+++ index.js
@@ -1,23 +1,57 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
+const { insertMany } = require("./models/Recipe.model");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://127.0.0.1:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
+
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+    Recipe.create({
+      title: "Asian Glazed Chicken Thighs",
+      level: "Amateur Chef",
+      ingredients: [
+        "1/2 cup rice vinegar",
+        "5 tablespoons honey",
+        "1/3 cup soy sauce (such as Silver Swan®)",
+        "1/4 cup Asian (toasted) sesame oil",
+        "3 tablespoons Asian chili garlic sauce",
+        "3 tablespoons minced garlic",
+        "salt to taste",
+        "8 skinless, boneless chicken thighs",
+      ],
+      cuisine: "Asian",
+      dishType: "main_course",
+      image:
+        "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+      duration: 40,
+      creator: "Chef LePapu",
+    })
+      .then((createdNewRecipe) => console.log(createdNewRecipe))
+      .catch((error) => console.log(error));
   });
+Recipe.insertMany(data)
+  .then((recipe) => console.log("Recipe title", data.title))
+  .catch((error) => console.log(error));
+
+Recipe.findOneAndUpdate({ duration: 220 }, { duration: 100 }).then((success) =>
+  console.log('success')
+);
+// Run your code here, after you have insured that the connection was made
+Recipe.deleteOne({title:'Carrot Cake'})
+.then((success) => console.log("success"))
+
+.catch((error) => {
+  console.error("Error connecting to the database", error);
+});
